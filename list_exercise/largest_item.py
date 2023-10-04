e6f527cffb0aad450ff9ebbf79d7c380dbc23afa
Ayinlade Sikiru <ayinladesikiru@gmail.com>
2023-10-04 21:32:29 +0100

    Update largest_item.py

--- list_exercise/largest_item.py
+++ list_exercise/largest_item.py
@@ -3,3 +3,6 @@ items_on_list = [15, 20, 25, 20, 10, 5]
 max_items_on_list = max(items_on_list)
 
 print(max(items_on_list))
+
+# dont use built in max function 
+# africa most celebrated cannot be lazy
